ac1abe63a42757d5e45b356b0d8d48d6535d99fe
Shorya Sharma <shoryasharma9560893769@gmail.com>
2026-03-13 23:27:18 +0530

    Add files via upload

---
+++ probelm3.py
@@ -0,0 +1,3 @@
+a=input("Enter value of a:")
+# b=float(a)
+print(type(a))
---
+++ type.py
@@ -0,0 +1,13 @@
+a=5
+t=type(a)  #int class
+print(t)
+
+b="75.4"
+d=type(b) #string class
+print(d)
+
+c="77.6"  #string class
+f=float(c)
+j=type(f)
+print(j)
+
