0f1c4ab03ae104d522077066b9e49be32bf8a721
Shorya Sharma <shoryasharma9560893769@gmail.com>
2026-04-08 19:27:51 +0530

    Delete problem_6.py

--- problem_6.py
+++
@@ -1,8 +0,0 @@
-# Write a program to mine a log file and find out whether it contains ‘python’.
-
-with open("log.txt") as f:
-    content=f.read()
-if ("python" in content):
-    print("YES python is present")
-else:
-    print("No python is not present")
