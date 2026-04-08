eb50a28d117bd195e2aec4719bd4e20f0ec71da6
Shorya Sharma <shoryasharma9560893769@gmail.com>
2026-04-08 19:28:28 +0530

    Delete problem_11.py

--- problem_11.py
+++
@@ -1,7 +0,0 @@
-# Write a python program to rename a file to “renamed_by_ python.txt.
-
-with open("self.txt") as f:
-    content=f.read()
-
-with open("renamed_by_pthon.txt" ,"w") as f:
-    f.write(content)
